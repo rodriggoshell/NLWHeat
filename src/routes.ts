3500fac93371f1f8e950a43aa406218fd194ca9c
rodriggoshell <rodriggoshell@gmail.com>
2021-10-20 01:42:29 -0300

    terminando

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,18 @@
 import { Router  } from 'express';
 import {AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController  } from './controllers/CreateMessageController';
+import { GetLastMessagesController } from './controllers/GetLastMessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated  } from './middleware/ensureAuthenticated';
 
 const router = Router();
 
 router.post('/authenticate', new AuthenticateUserController().handle);
 
-router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
+
+router.get('/last', new GetLastMessagesController().handle);
+
+router.get('/profile',ensureAuthenticated, new ProfileUserController().handle);
 
 export { router }
